32407d8e99660ccc96cae11dda4a43d7c4b0b271
Divya <divyadivyashankar011@gmail.com>
2026-08-11 21:44:25 +0530

    updated lib/main.dart

--- sumathi_style_app/lib/main.dart
+++ sumathi_style_app/lib/main.dart
@@ -169,7 +169,7 @@ class _WebViewScreenState extends State<WebViewScreen> {
     await controller.clearLocalStorage();
     await controller.loadRequest(
       Uri.parse(
-        'http://192.168.1.5/style/index.html'
+        'https://sumathisstyles.vercel.app/'
       ),
     );
   }
