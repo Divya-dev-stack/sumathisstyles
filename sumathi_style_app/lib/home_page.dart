bf38ac4e5f96d3d248a8f710d86e3a21261dc6e5
Divya <divyadivyashankar011@gmail.com>
2026-08-18 18:33:37 +0530

    updated settings.html

--- sumathi_style_app/lib/home_page.dart
+++ sumathi_style_app/lib/home_page.dart
@@ -13,7 +13,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   late final WebViewController _controller;
 
-  final Color teal = const Color.fromARGB(255, 200, 211, 210);
+  final Color teal = const Color.fromARGB(255, 249, 250, 250);
 
   @override
   void initState() {
